11494603ab8450697997fa15ab58515cf94bad02
Devesh <devesh.lingayat125@gmail.com>
2024-01-12 19:25:38 +0530

    basics of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+import Chai from "./Chai";
+function App() {
+  return (
+    <h1><Chai/></h1>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+function Chai(){
+    return (
+        <>
+        <h2>Hello world chai is love</h2>
+        </>
+    );
+}
+export default Chai;
